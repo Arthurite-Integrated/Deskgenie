f10d23a6e4979f98fe01e6410c315cbdad8570d9
sp3c7r4 <sp3c7r40x00@gmail.com>
2025-08-17 09:01:04 +0100

    feat: prod push hard coded

--- src/app/utils/user.controller.ts
+++ src/app/utils/user.controller.ts
@@ -5,7 +5,7 @@ import env from "../config/env";
 
 export async function getSessionToken() {
   const openai = new OpenAI({
-    apiKey: env.NEXT_OPENAI_API_KEY,
+    apiKey: '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',
     dangerouslyAllowBrowser: true,
   });
 
